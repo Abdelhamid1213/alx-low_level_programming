04625c9663c391d240e8521f8d8e91419a80651a
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-06-16 10:58:32 +0100

    task 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: use putchar to prints all possible combinations of single-digit numbers.
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void) 
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x + 48);
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++x;	
+	}
+	putchar('\n');
+	return (0);
+}
