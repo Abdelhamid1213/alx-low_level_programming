4ff9b5d3f6632ed9a6f7c3d902a5ad4da97711f6
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-06-16 10:43:23 +0100

    task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 102)
+	{
+		putchar(x);
+		if (x == 57)
+			x += 39;
+			++x;
+	}
+	putchar('\n');
+	return (0);
+}
