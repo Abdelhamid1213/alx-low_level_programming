ea0bcd83d55aec654736d49c001a57ea09615ab3
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-06-20 11:49:52 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - use putchar to print alphabets 10 times.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0, i <= 10, i++)
+	{
+		int c;
+
+		for (c = 'a', c <= 'z', c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
