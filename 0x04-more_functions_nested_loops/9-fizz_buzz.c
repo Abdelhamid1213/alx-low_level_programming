2486184b11d9847cd0259a2cf0105770e3bdcc1c
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-06-22 20:19:33 +0100

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: FizzBuzz Program
+ *
+ * Return: Always 0
+ */
+
+int main(void);
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
