f5b255912833e9e6456a469290a95929a7a57dea
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-06-26 16:30:50 +0100

    advanced task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,12 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
+	unsigned int result = 0;
 	int sign = 1;
 
 	do {
 		if (*s == '-')
-			sign = -1;
+			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			result = (result * 10) + (*s - '0');
 		else if (result > 0)
