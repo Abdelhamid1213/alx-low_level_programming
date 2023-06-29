536046cdb32a7613d3f6cb342e3b18c56863ef84
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-06-29 19:28:00 +0100

    update tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,20 +9,16 @@
 
 void print_number(int n)
 {
-	int i, p;
+	unsigned int num = n;
 
 	if (n < 0)
-		_putchar('-');
-	for (p = 0, i = 1000000000; i > 0; i /= 10)
 	{
-		int d = (n / i) % 10;
-
-		d = d < 0 ? -d : d;
-		if (d || p || i == 1)
-		{
-			_putchar(d + '0');
-			p++;
-		}
-
+		_putchar('-');
+		num = -num;
 	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
