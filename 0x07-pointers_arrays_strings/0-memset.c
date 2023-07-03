e9dd0efc246632ee0330845ec450f2610907ac0e
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-03 12:07:21 +0100

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
+	{
+		*s = b;
+		s++;
+	}
 
-	return (s)
+	return (s);
 }
