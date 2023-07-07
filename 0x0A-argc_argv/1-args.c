63ae2b1ab4228d209c11d937a60ff9a426de97ca
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-07 16:36:05 +0100

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: number
+ * @argv: array of strings
+ *
+ * Return: 0 Always success.
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
