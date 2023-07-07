783f290a33ba586f7b240030aceadb4a67925901
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-07 14:38:53 +0100

    Task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - function that return absolute value of an integer.
+ *
+ * @n: integer.
+ *
+ * Return: possitive value of int.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+	return (n);
+}
