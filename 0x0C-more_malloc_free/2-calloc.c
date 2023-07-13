5db45e25f03e5c144d7a1cdf0959d6a2bf981a34
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-13 11:01:56 +0100

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int i;
-    char *array;
+	unsigned int i;
+	char *array;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    array = malloc(nmemb * size);
-    if (array == NULL)
-        return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0; i < nmemb * size; i++)
-        array[i] = 0;
-    return (array);
+	for (i = 0; i < nmemb * size; i++)
+		array[i] = 0;
+	return (array);
 }
