f2ac9c11b1e36aae3f2f36d4a3e675c96063fdb4
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-19 12:04:13 +0100

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+/**
+ * op_add - add two numbers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result 
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract two numbers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result 
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two numbers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result 
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numbers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result 
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - return modulos of two numbers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result 
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
