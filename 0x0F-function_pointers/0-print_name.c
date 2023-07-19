122c8f371ce945cfd791d8b503758ce132e343d5
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-19 08:25:50 +0100

    Initial Commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ *
+ * @name: string to print
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    
+}
