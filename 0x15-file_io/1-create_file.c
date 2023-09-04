45341b02f17b273a2c4cbe144619d2cbd05c1868
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-09-04 11:45:44 +0100

    15

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: file to create
+ * @text_content: content of file
+ * Return: 1 on sucess else -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+
+	if (!filename)
+		return (-1);
+
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (f == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		ssize_t text_len = strlen(text_content);
+		ssize_t bytes_written = write(f, text_content, text_len);
+
+		if (bytes_written != text_len)
+		{
+			close(f);
+			return (-1);
+		}
+	}
+
+	close(f);
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+#define MAXSIZE 1024
+
+
+/**
+ * exit_cp - ..
+ * @error: ..
+ * @str: ..
+ * @fd: ..
+ * Return: ..
+ */
+
+int exit_cp(int error, char *str, int fd)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+			exit(error);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+			exit(error);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(error);
+		default:
+			return (0);
+	}
+}
+
+/**
+ * main - ..
+ * @argc: ..
+ * @argv: ..
+ * Return: ..
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_in, file_out;
+	int read_stat, write_stat;
+	int close_in, close_out;
+	char buffer[MAXSIZE];
+
+	if (argc != 3)
+		exit_cp(97, NULL, 0);
+
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+		exit_cp(98, argv[1], 0);
+
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		exit_cp(99, argv[2], 0);
+
+	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
+	{
+		if (read_stat == -1)
+			exit_cp(98, argv[1], 0);
+
+		write_stat = write(file_out, buffer, read_stat);
+		if (write_stat == -1)
+			exit_cp(99, argv[2], 0);
+	}
+
+	close_in = close(file_in);
+	if (close_in == -1)
+		exit_cp(100, NULL, file_in);
+
+	close_out = close(file_out);
+	if (close_out == -1)
+		exit_cp(100, NULL, file_out);
+
+	return (0);
+}
