8606744d9c76d5b640f87e03a931f4cb06315a9d
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-10-12 23:30:40 +0100

    python code to find the palindrom

--- 0x17-doubly_linked_lists/10-palindromes.py
+++ 0x17-doubly_linked_lists/10-palindromes.py
@@ -7,4 +7,9 @@ for i in range(0, 999):
         if isPalindrom(i * j):
             palindroms.add(i * j)
 
-print(max(palindroms))
+print('largest palindrome made from the product \
+of two 3-digit numbers is {}'.format(max(palindroms)))
+
+f = open("102-result", "w")
+f.write(str(max(palindroms)))
+f.close
