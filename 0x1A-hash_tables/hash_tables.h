6fb7daed17bf09995cc665c0abf36192dc23cbb3
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-11-03 15:44:03 +0100

    Task 5

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,6 +40,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int handle_collision(hash_table_t *ht, unsigned long int index, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 
 
 #endif /* HASH_TABLES_H */
