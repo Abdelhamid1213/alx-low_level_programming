e679fb31acca16ddadd5142d7befe9571e2ec3e0
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-11-03 15:10:47 +0100

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i;
 
 	if (size <= 0)
 		return (NULL);
@@ -21,6 +22,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (!hash_table->array)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 
 	return (hash_table);
 }
