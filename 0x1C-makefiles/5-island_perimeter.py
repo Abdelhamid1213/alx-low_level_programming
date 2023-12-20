19582e43507a0768f2dec2d2eb7933c7f919565a
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-12-20 19:44:55 +0100

    Tasks

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,7 +8,6 @@ def island_perimeter(grid):
     perimeter = 0
     for i, _ in enumerate(grid):
         for j, _ in enumerate(grid[i]):
-            print(grid[i][j], end=',')
             if grid[i][j] == 1:
                 perimeter += 1 if grid[i][j+1] == 0 else 0
                 perimeter += 1 if grid[i][j-1] == 0 else 0
