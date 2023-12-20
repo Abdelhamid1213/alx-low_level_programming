3e3edede106431d800fd6616d0176fba33c8c663
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-12-20 19:43:20 +0100

    Tasks

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
+""".."""
 def island_perimeter(grid):
+    """..."""
     perimeter = 0
     for i, _ in enumerate(grid):
         for j, _ in enumerate(grid[i]):
             print(grid[i][j], end=',')
             if grid[i][j] == 1:
-                if grid[i][j]
-
-
-
-
-
-"""
-grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
-    """
-grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-island_perimeter(grid)
+                perimeter += 1 if grid[i][j+1] == 0 else 0
+                perimeter += 1 if grid[i][j-1] == 0 else 0
+                perimeter += 1 if grid[i+1][j] == 0 else 0
+                perimeter += 1 if grid[i-1][j] == 0 else 0
+    return perimeter
